ecfb6b4909b63720347760b26799dad8d359c92b
Rafael <rafael.uribeg@gmail.com>
2024-04-18 21:45:32 -0400

    Initial commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Bookmarck = () => {
+  return (
+    <View>
+      <Text>Bookmarck</Text>
+    </View>
+  )
+}
+
+export default Bookmarck
